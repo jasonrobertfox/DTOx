49a5fd1fc697a2928ccd589e21ced6facd6881ef
Jason Fox <jasonrobertfox@gmail.com>
2014-03-24 18:14:17 -0300

    Attempt to fix pagoda issue.

--- after_build.sh
+++ after_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+    #!/bin/bash
 PHP=php
 
 echo 'Download the composer.phar file, so the vendors can be installed from the distributed composer.json.'
@@ -8,10 +8,10 @@ if [ ! -f composer.phar ]
 fi
 
 echo 'Update composer.phar.'
-$PHP composer.phar self-update --quiet
+$PHP composer.phar self-update
 
 echo 'Install the needed vendors for this application.'
-$PHP composer.phar install --quiet
+$PHP composer.phar install
 
 echo 'Dump the optimized autoloader classmap for performance reasons.'
 $PHP composer.phar dump-autoload --optimize
